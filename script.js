3bed42e468f4877cddd4bee9a4b2b7c615599552
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-05-04 12:33:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const element = document.getElementById('level');
+let n = 0;
+
+while (element.parentNode) {
+  n++;
+  element = element.parentNode;
+}
+
+alert("The level of the element is:"+n);
